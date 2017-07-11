9b0fd709a9e7305ca3d0a527b6f94b40f98692d2
Jenny <jennys24@in.ibm.com>
2017-07-11 18:53:22 +0530

    Cat added

---
+++ src/main/java/com/farm/entity/Category.java
@@ -0,0 +1,27 @@
+package com.farm.entity;
+
+public class Category {
+	
+private Integer catId;
+private String catName;
+private String catDesc;
+public Integer getCatId() {
+	return catId;
+}
+public void setCatId(Integer catId) {
+	this.catId = catId;
+}
+public String getCatName() {
+	return catName;
+}
+public void setCatName(String catName) {
+	this.catName = catName;
+}
+public String getCatDesc() {
+	return catDesc;
+}
+public void setCatDesc(String catDesc) {
+	this.catDesc = catDesc;
+}
+
+}
