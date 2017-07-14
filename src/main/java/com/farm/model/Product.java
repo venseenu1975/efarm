358d7cbfbe9d4d098a7c524971929104a8bb421d
Usha -IBM <IBM@172.19.44.5>
2017-07-14 12:46:46 +0530

    New objects for new product/category functionality

---
+++ src/main/java/com/farm/model/Product.java
@@ -0,0 +1,49 @@
+package com.farm.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Product {
+
+	private Integer id;
+	MultipartFile imgFile;
+	private Integer categoryId;
+	private String name;
+	private String image;
+	private String description;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public MultipartFile getImgFile() {
+		return imgFile;
+	}
+	public void setImgFile(MultipartFile imgFile) {
+		this.imgFile = imgFile;
+	}
+}
