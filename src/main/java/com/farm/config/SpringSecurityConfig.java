1f7c15cbaab14f914cd4c9a843a78b3a459d5e36
Jenny <jennys24@in.ibm.com>
2017-07-24 12:00:16 +0530

    home page image display fix

--- src/main/java/com/farm/config/SpringSecurityConfig.java
+++ src/main/java/com/farm/config/SpringSecurityConfig.java
@@ -36,7 +36,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-	    web.ignoring().antMatchers("/css/**");
+	    web.ignoring().antMatchers("/css/**","/images/**","/scripts/**");
 	}
 	
     @Override
